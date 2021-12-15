e924b53e081a19e98fe86c6f1c36affb8cca378c
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-15 13:14:18 +0100

    Added an if statement for users to upload images

--- app/users/register.php
+++ app/users/register.php
@@ -6,4 +6,12 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
 
-redirect('/');
+if (isset($_FILES['avatar'])) {
+    $avatar = $_FILES['avatar'];
+    $destination = __DIR__ . '/../profileImages/' . date("y-m-d") . $avatar['name'];
+    move_uploaded_file($avatar['tmp_name'], $destination);
+    $message = 'The file was successfully uploaded!';
+}
+
+
+// redirect('/');
