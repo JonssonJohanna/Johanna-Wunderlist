85b7901f65aba92a4ddc5e706d1efdb0e25ad66d
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-15 15:55:43 +0100

    If statement, PHP to add in to database

--- app/users/register.php
+++ app/users/register.php
@@ -6,12 +6,26 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
 
+
+// To add users uploaded images
 if (isset($_FILES['avatar'])) {
     $avatar = $_FILES['avatar'];
     $destination = __DIR__ . '/../profileImages/' . date("y-m-d") . $avatar['name'];
     move_uploaded_file($avatar['tmp_name'], $destination);
     $message = 'The file was successfully uploaded!';
 }
+
+// To add a users name, not sanitized
+if (isset($_POST['name'])) {
+    $name = trim($_POST['name']);
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    // $image = $_FILES['avatar'];
+
+    $database = new PDO('sqlite:database.db');
+    $database->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
+}
+
 
 
 // redirect('/');
