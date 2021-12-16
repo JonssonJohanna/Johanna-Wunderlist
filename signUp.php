7e2de200ce8fb395941525090ac28fecdf36f7a4
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-16 15:30:02 +0100

    Removed name and image from form

--- signUp.php
+++ signUp.php
@@ -6,12 +6,12 @@
     <h1>Become a member on this page</h1>
 
     <form action="app/users/register.php" method="post" enctype="multipart/form-data">
-
-        <div class="mb-3">
+        <!-- This should be in it own form -->
+        <!-- <div class="mb-3">
             <label for="name">Name</label>
             <input class="form-control" type="name" name="name" id="name" placeholder="Arthur Meland" required>
             <small class="form-text">Write your name here.</small>
-        </div>
+        </div> -->
         <div class="mb-3">
             <label for="email">Email</label>
             <input class="form-control" type="email" name="email" id="email" placeholder="arthur.meland@lo.com" required>
@@ -23,12 +23,12 @@
             <input class="form-control" type="password" name="password" id="password" required>
             <small class="form-text">Please provide the your password (passphrase).</small>
         </div>
-
-        <div>
+        <!-- This should be in it own form -->
+        <!-- <div>
             <label for="avatar">Upload optional image here</label>
             <input type="file" name="avatar" id="avatar" accept=".jpg, .png" required>
 
-        </div>
+        </div> -->
 
 
         <button type="submit" class="btn btn-info">Sign up</button>
