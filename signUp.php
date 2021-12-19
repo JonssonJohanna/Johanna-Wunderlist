f994e36104416c4fdd22fcbcd47a35ceb37e314a
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-19 18:58:33 +0100

    Updated h1 in signUp

--- signUp.php
+++ signUp.php
@@ -3,15 +3,15 @@
 
 
 <article>
-    <h1>Become a member on this page</h1>
+    <h1>Create an account</h1>
 
     <form action="app/users/register.php" method="post" enctype="multipart/form-data">
         <!-- This should be in it own form -->
-        <!-- <div class="mb-3">
+        <div class="mb-3">
             <label for="name">Name</label>
             <input class="form-control" type="name" name="name" id="name" placeholder="Arthur Meland" required>
             <small class="form-text">Write your name here.</small>
-        </div> -->
+        </div>
         <div class="mb-3">
             <label for="email">Email</label>
             <input class="form-control" type="email" name="email" id="email" placeholder="arthur.meland@lo.com" required>
@@ -23,12 +23,12 @@
             <input class="form-control" type="password" name="password" id="password" required>
             <small class="form-text">Please provide the your password (passphrase).</small>
         </div>
-        <!-- This should be in it own form -->
-        <!-- <div>
-            <label for="avatar">Upload optional image here</label>
-            <input type="file" name="avatar" id="avatar" accept=".jpg, .png" required>
+        <!-- This should be in ist own form -->
+        <div>
+            <label for="profile_image">Upload optional image here</label>
+            <input type="file" name="profile_image" id="profile_image" accept=".jpg, .png" required>
 
-        </div> -->
+        </div>
 
 
         <button type="submit" class="btn btn-info">Sign up</button>
