6c3b59d4947947dd1b68b8ce13af41cf47b781f6
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-20 19:12:11 +0100

    Removed the second form

--- signUp.php
+++ signUp.php
@@ -5,8 +5,8 @@
 <article>
     <h1>Create an account</h1>
 
-    <form action="app/users/register.php" method="post">
-        <!-- This should be in it own form -->
+    <form action="app/users/register.php" method="post" enctype="multipart/form-data">
+        This should be in it own form
         <div class="mb-3">
             <label for="name">Name</label>
             <input class="form-control" type="name" name="name" id="name" placeholder="Arthur Meland" required>
@@ -23,17 +23,10 @@
             <input class="form-control" type="password" name="password" id="password" required>
             <small class="form-text">Please provide the your password (passphrase).</small>
         </div>
-        <!-- This should be in ist own form -->
-        <form action="app/users/images.php" method="post" enctype="multipart/form-data">
-            <div>
-                <label for="profile_image">Upload optional image here</label>
-                <input type="file" name="profile_image" id="profile_image" accept=".jpg, .png" required>
-
-            </div>
 
 
-            <button type="submit" class="btn btn-info">Sign up</button>
-        </form>
+        <button type="submit" class="btn btn-info">Sign up</button>
+
     </form>
 </article>
 
