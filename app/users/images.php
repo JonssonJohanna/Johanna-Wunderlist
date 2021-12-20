a0c5b6455b83f0c1ba736ecde1eac2c1f907dedf
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-20 16:51:52 +0100

    Changed image.php removed parts that was not in use

--- app/users/images.php
+++ app/users/images.php
@@ -4,21 +4,21 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-// To add users uploaded images
+// add users uploaded images in 'uploads' and in database
 if (isset($_FILES['profile_image'])) {
-    $destination = __DIR__ . '/../../uploads/' . date("y-m-d") . $avatar;
-    $avatarTemp = $_FILES['profile_image']['tmp_name'];
-    move_uploaded_file($avatarTemp, $destination);
-}
+    //saving image in filesystem
+    $avatar = trim(filter_var($_FILES['profile_image'], FILTER_SANITIZE_STRING));
+    $newAvatar = date("y-m-d H-i-s") . $avatar['name'];
+    $destination =  __DIR__ . '/../../uploads/' . $newAvatar;
+    move_uploaded_file($avatar['tmp_name'], $destination);
+    $alert = 'The file is uploaded';
 
+    // Adds a row in database
+    $insertSQL = ("UPDATE users SET profile_image = :profile_image WHERE id = :id");
 
-if (isset($_FILES['profile_image'])) {
-    $avatar = trim(filter_var($_FILES['profile_image']['name'], FILTER_SANITIZE_STRING));
-    $insertSQL = ("INSERT INTO users ('profile_image') VALUES (:profile_image)");
     $sql = $database->prepare($insertSQL);
-
-    $sql->bindParam(':profile_image', $avatar, PDO::PARAM_STR);
-
+    $sql->bindParam(':profile_image', $newAvatar, PDO::PARAM_STR);
+    $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
     $sql->execute();
 };
 
