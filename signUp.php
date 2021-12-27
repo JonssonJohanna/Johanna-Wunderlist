0cde71abe1ec6da84152e1ea5587e45e89e76f2d
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-27 21:04:09 +0100

    added session "message" messeage alert when input field is empty

--- signUp.php
+++ signUp.php
@@ -1,27 +1,41 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php';  ?>
 
+<?php
+if (!isset($_SESSION['messages'])) {
+    $messages = "";
+} else {
+    $messages = $_SESSION['messages'];
+    unset($_SESSION['messages']);
+}
+?>
 
 <article>
     <h1>Create an account</h1>
+    <p><?= $messages; ?></p>
 
     <form action="app/users/register.php" method="post" enctype="multipart/form-data">
-        This should be in it own form
+
         <div class="mb-3">
             <label for="name">Name</label>
-            <input class="form-control" type="name" name="name" id="name" placeholder="Arthur Meland" required>
+            <input class="form-control" type="name" name="name" id="name" placeholder="Arthur Meland">
             <small class="form-text">Write your name here.</small>
         </div>
         <div class="mb-3">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="arthur.meland@lo.com" required>
+            <input class="form-control" type="email" name="email" id="email" placeholder="arthur.meland@lo.com">
             <small class="form-text">Please provide the your email address.</small>
         </div>
 
         <div class="mb-3">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text">Please provide the your password (passphrase).</small>
+            <small class="form-text">Please provide your password (passphrase).</small>
+        </div>
+        <div class="mb-3">
+            <label for="password">RepeatPassword</label>
+            <input class="form-control" type="password" name="repeatPassword" id="password" required>
+            <small class="form-text">Repeat password.</small>
         </div>
 
 
