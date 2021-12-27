901282906771ca491f8392be6b68378bb73e71ae
JonssonJohannae <johanna_jonson@hotmail.com>
2021-12-27 21:00:38 +0100

    users can update their email, and added in to database in email.php

--- app/users/update/email.php
+++ app/users/update/email.php
@@ -7,19 +7,17 @@ require __DIR__ . '/../../autoload.php';
 
 //user editinig email
 
-if (isset($_POST['email'], $_POST['newEmail'])) {
+if (isset($_POST['newEmail'])) {
     $newEmail = trim(filter_var($_POST['newEmail'], FILTER_VALIDATE_EMAIL));
 
-    $insertSql = $database->prepare("UPDATE users SET email = :email WHERE id = :id;");
-    $sql = $database->prepare($insertSQL);
-    $sql->bindParam(':newEmail', $newEmail, PDO::PARAM_STR);
+    $insertSql = ("UPDATE users SET email = :email WHERE id = :id;");
+    $sql = $database->prepare($insertSql);
+    $sql->bindParam(':email', $newEmail, PDO::PARAM_STR);
     $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
-    $sql->execute();
-    $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+    // $sql->execute();
+    // $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
 
     $sql->execute();
-
-    $_SESSION['user'] = $sql->fetch(PDO::FETCH_ASSOC);
 };
 //else {
 //     header("Location: index.php");
@@ -35,4 +33,4 @@ if (isset($_POST['email'], $_POST['newEmail'])) {
 //     $_SESSION['email'] = $sql->fetch(PDO::FETCH_ASSOC);
 // }
 
-redirect('/');
+redirect('/index.php');
