e98392fa471964aa6d2b185e47049e02d75ed90c
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-03 13:23:26 +0000

    Test: added a table form for adding tasks

--- lists.php
+++ lists.php
@@ -7,13 +7,44 @@
     </form>
     <h2>Create Lists</h2>
     <form action="/app/users/todo/lists.php" method="post">
-        <div class="mb-3">
+        <div class="mb-3 listForm">
             <label for="title">List name</label>
             <input class="form-control" type="name" name="title" id="title" placeholder="write list name here" required>
             <small class="form-text">Please fill in your task name.</small>
         </div>
-        <button type="submit" class="btn btn-info">New list title</button>
+        <button type="submit" name="submit" class="btn btn-info">New list title</button>
+        <br>
+        <table>
+            <thead>
+                <tr>
+                    <th>N</th>
+                    <th>Task</th>
+                    <th>Description</th>
+                    <th>Date</th>
+                    <th>Check</th>
+                </tr>
+            </thead>
+            <thead>
+                <tr>
+                    <td>1</td>
+                    <td class="task">Task name</td>
+                    <td class="description">Describe task here</td>
+                    <td><input type="date" id="deadline" name="deadline"></td>
+                    <td class="delete">
+                        <a href="#">X</a>
+                    </td>
+                </tr>
+            </thead>
+        </table>
+
+        <?php
+        // foreach ($title as $row)
+        ?>
+        <!-- <option><?php echo $row['title'] ?> -->
+        </option>
+
     </form>
 </article>
+
 
 <?php require __DIR__ . '/views/footer.php'; ?>
