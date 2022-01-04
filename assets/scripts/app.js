f9f632302aade1555365ffc1e9cc18e3963e08f9
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-04 17:02:27 +0100

    edited js function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const taskButtons = document.querySelectorAll('.taskButton');
+const hiddenForm = document.querySelector('.hiddenForm');
 
 taskButtons.forEach((taskButton) => {
   taskButton.addEventListener('click', () => {
