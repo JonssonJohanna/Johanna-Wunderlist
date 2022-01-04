9d741eb3baa4a467abc880857763ff353f874a1e
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-04 12:05:22 +0100

    removed form from tasks.php

--- tasks.php
+++ tasks.php
@@ -1,31 +1,6 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
-    <h1>Edit your tasks here</h1>
 
-    </form>
-    <h2>Create tasks</h2>
-    <form action="/app/users/todo/tasks.php" method="post">
-        <div class="mb-3">
-            <label for="title">Tasks</label>
-            <input class="form-control" type="name" name="title" id="title" placeholder="write task title here" required>
-            <small class="form-text">Please fill in your task name.</small>
-        </div>
-        <div class="mb-3">
-            <label for="tasks">Description</label>
-            <input class="form-control" type="tasks" name="tasks" id="tasks" placeholder="write tasks here" required>
-            <small class="form-text">Describe your task.</small>
-        </div>
-        <div class="mb-3">
-            <label for="deadline">Deadline</label>
-            <input class="form-control" type="date" name="deadline" id="deadline" placeholder="write ">
-            <small class="form-text">Please fill in deadline for task.</small>
-        </div>
-        <button type="submit" class="btn btn-info">Add new task</button>
-    </form>
-
-
-</article>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
