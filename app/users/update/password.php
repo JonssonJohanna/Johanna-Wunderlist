2d5c5fc9c426b82f3710095e83e96cc36eb457e1
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-04 12:04:03 +0100

    Edited error messages

--- app/users/update/password.php
+++ app/users/update/password.php
@@ -5,28 +5,31 @@ declare(strict_types=1);
 require __DIR__ . '/../../autoload.php';
 
 if (isset($_POST['newPassword'])) {
-    $newPassword = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);;
+    $newPassword = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
+    $confirmNewPassword = password_hash($_POST['confirmNewPassword'], PASSWORD_DEFAULT);
 
-    $insertSql = ("UPDATE users SET password = :password WHERE id = :id;");
+    // ett meddelande som skrivs ut i den andra edit.php ifall man har bytt e-post, om SESSION emailMessage isset.
+
+    if ($_POST['newPassword'] !== $_POST['confirmNewPassword']) {
+        $_SESSION['messageError'] = "Passwords do not match";
+        redirect("/profile.php");
+    }
+
+    $insertSql = ("UPDATE users SET password = :password WHERE id = :id");
+
     $sql = $database->prepare($insertSql);
+
     $sql->bindParam(':password', $newPassword, PDO::PARAM_STR);
+
     $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
-    // $sql->execute();
-    // $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+
 
     $sql->execute();
 
-    $_SESSION['user'] = $sql->fetch(PDO::FETCH_ASSOC);
-
-    // ett meddelande som skrivs ut i den andra edit.php ifall man har bytt e-post, om SESSION emailMessage isset.
     $_SESSION['messagePassword'] = "You have succsessfully changed your password";
 
-    if ($newPassword !== $confirmNewPassword) {
-        $_SESSION['messageError'] = "Passwords does not match";
-        header("Location: /../../profile.php");
-        exit;
-    }
-};
+    // $_SESSION['user'] = $sql->fetch(PDO::FETCH_ASSOC);
+}
 
 
 
