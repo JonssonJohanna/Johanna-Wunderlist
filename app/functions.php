e365c22c1da71d47fb9c13f42387ffb7cf5e71a3
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-04 16:06:12 +0100

    created a function for displaying lists

--- app/functions.php
+++ app/functions.php
@@ -7,3 +7,16 @@ function redirect(string $path)
     header("Location: ${path}");
     exit;
 }
+
+// function that adds new list and displays it
+
+function addLists(PDO $database): array
+{
+    $sql = $database->prepare('SELECT * FROM lists WHERE user_id = :id');
+    $sql->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+
+    $sql->execute();
+
+    $lists = $sql->fetchAll(PDO::FETCH_ASSOC);
+    return $lists;
+}
