a5dfda4dab650641bbbe046d78722f90cf976f66
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-04 12:04:54 +0100

    added task form to same page as list form

--- lists.php
+++ lists.php
@@ -2,7 +2,6 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
 <article>
-    <h1>Lists</h1>
 
     </form>
     <h2>Create Lists</h2>
@@ -10,41 +9,67 @@
         <div class="mb-3 listForm">
             <label for="title">List name</label>
             <input class="form-control" type="name" name="title" id="title" placeholder="write list name here" required>
-            <small class="form-text">Please fill in your task name.</small>
         </div>
-        <button type="submit" name="submit" class="btn btn-info">New list title</button>
+        <button type="submit" name="submit" class="btn btn-info">Create list</button>
         <br>
         <table>
             <thead>
                 <tr>
-                    <th>N</th>
+                    <th>List</th>
                     <th>Task</th>
                     <th>Description</th>
                     <th>Date</th>
-                    <th>Check</th>
+                    <th>✔️</th>
                 </tr>
             </thead>
             <thead>
                 <tr>
-                    <td>1</td>
-                    <td class="task">Task name</td>
+                    <td>List</td>
+                    <td class="task">Task</td>
                     <td class="description">Describe task here</td>
-                    <td><input type="date" id="deadline" name="deadline"></td>
-                    <td class="delete">
+                    <td><input type="date" name="deadline"></td>
+                    <td class="check">
                         <a href="#">X</a>
                     </td>
                 </tr>
             </thead>
         </table>
 
-        <?php
-        // foreach ($title as $row)
-        ?>
-        <!-- <option><?php echo $row['title'] ?> -->
-        </option>
+    </form>
+    <?php
+
+    foreach ($title as $list);
+    $list['title']; ?>
+
+    <div>
+        <p>
+        <h2 </p>
+    </div>
 
     </form>
+    <br>
+    <form class="hiddenForm" action="/app/users/todo/tasks.php" method="post">
+        <div class="mb-3">
+            <label for="title">Tasks</label>
+            <input class="form-control" type="name" name="title" id="title" placeholder="write task title here" required>
+            <small class="form-text">Please fill in your task name.</small>
+        </div>
+        <div class="mb-3">
+            <label for="tasks">Description</label>
+            <input class="form-control" type="tasks" name="tasks" id="tasks" placeholder="write tasks here" required>
+            <small class="form-text">Describe your task.</small>
+        </div>
+        <div class="mb-3">
+            <label for="deadline">Deadline</label>
+            <input class="form-control" type="date" name="deadline" id="deadline" placeholder="write ">
+            <small class="form-text">Please fill in deadline for task.</small>
+        </div>
+        <button class="toogleButton" type="submit" class="btn btn-info">Add new task</button>
+    </form>
+
+
 </article>
+
 
 
 <?php require __DIR__ . '/views/footer.php'; ?>
