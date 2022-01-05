7b16932af5b87cf6aa34e330e78fb917150d265e
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-05 18:27:55 +0100

    js functions that are not in use anymore

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,16 @@
-const taskButtons = document.querySelectorAll('.taskButton');
+const listsButton = document.querySelector('.listsButton');
+const taskButtons = document.querySelectorAll('.tasksButton');
 const hiddenForm = document.querySelector('.hiddenForm');
+const hiddenTable = document.querySelector('.hiddenTable');
 
-taskButtons.forEach((taskButton) => {
-  taskButton.addEventListener('click', () => {
-    hiddenForm.classList.toggle('on');
-  });
-});
+// taskButtons.forEach((taskButton) => {
+//   taskButton.addEventListener('click', () => {
+//     hiddenForm.classList.toggle('on');
+//   });
+// });
+// // Toggles between showing an not showing table for adding tasks
+// taskButtons.forEach((taskButton) => {
+//   taskButton.addEventListener('click', () => {
+//     hiddenTable.classList.toggle('tableOn');
+//   });
+// });
