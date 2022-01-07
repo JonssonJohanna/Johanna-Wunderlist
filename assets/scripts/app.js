7090c55111e6ef8b6777cb7d6ff13a5ad194f2bd
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-07 13:19:35 +0100

    added js eventlistener for submitting form

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,8 @@
-// const listsButton = document.querySelector('.listsButton');
-// const taskButtons = document.querySelectorAll('.tasksButton');
-// const hiddenForm = document.querySelector('.hiddenForm');
-// const hiddenTable = document.querySelector('.hiddenTable');
-
-// taskButtons.forEach((taskButton) => {
-//   taskButton.addEventListener('click', () => {
-//     hiddenForm.classList.toggle('on');
-//   });
-// });
-// // Toggles between showing an not showing table for adding tasks
-// taskButtons.forEach((taskButton) => {
-//   taskButton.addEventListener('click', () => {
-//     hiddenTable.classList.toggle('tableOn');
-//   });
-// });
-
 // function lists tasks with deadline in ASC/DESC
+
+// function submit form for edit lists
+const submitList = document.querySelectorAll($list['id']);
+document.querySelector('.submit'),
+  addEventListener('click', function () {
+    submitList.submit();
+  });
