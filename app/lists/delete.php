2cb836b138b296e016f2e11eb1c658d0ebf4f7c8
JonssonJohannae <johanna_jonson@hotmail.com>
2022-01-07 14:07:05 +0100

    added a delete list logic

--- app/lists/delete.php
+++ app/lists/delete.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../../autoload.php';
+require __DIR__ . '/../autoload.php';
+
+// The task ID allows to delete the specifik task -> connected with form in create.php
+
+if (isset($_POST['id'])) {
+    $list = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+    $insertSQL = $database->prepare('DELETE FROM lists WHERE id = :id');
+    $insertSQL->bindParam(':id', $list, PDO::PARAM_INT);
+    $insertSQL->execute();
+}
 
 redirect('/../../create.php');
